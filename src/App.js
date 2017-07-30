b7808608e185701826df1b3963f77226dfb9de9e
damiepsz3 <personal@AFS-ML-105.home>
2017-07-29 23:46:19 -0400

    refactor: Split the existence component in more components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import BookShelf from './BookShelf'
 
-class BooksApp extends React.Component {
+class App extends React.Component {
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -13,6 +14,12 @@ class BooksApp extends React.Component {
     showSearchPage: true
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+        console.log(books);
+    })
+  }
+
   render() {
     return (
       <div className="app">
@@ -21,16 +28,16 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
-                {/* 
+                {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
+
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
                 <input type="text" placeholder="Search by title or author"/>
-                
+
               </div>
             </div>
             <div className="search-books-results">
@@ -44,6 +51,7 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
+                <BookShelf title='Currently Reading'/>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
@@ -203,4 +211,4 @@ class BooksApp extends React.Component {
   }
 }
 
-export default BooksApp
+export default App
