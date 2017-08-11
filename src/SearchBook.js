d84cfdc202780fcba92a764fcd69fd31d8c99d4e
damienepsztein <damian.epsztein@afs.org>
2017-08-11 15:40:56 -0400

    feat: Wait until stop typing to search

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -12,22 +12,31 @@ class SearchBook extends Component {
 
   state = {
       results: [],
-      query: ''
+      query: '',
+      searching: null
+  }
+
+  componentDidMount () {
+    this.timer = null;
   }
 
   updateBooks = (query) => {
-    this.setState({ query })
-    BooksAPI.search(query, 20)
-    .then((results) => {
-        this.setState({ results: (results && !results.error ? results : []) } )
-    })
-    .then((message) => console.log(`There was an error, message: ${message}`))
+    this.setState({ query,
+                    searching: (query.length > 0 ? true : false) })
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      BooksAPI.search(query, 20)
+      .then((results) => {
+          this.setState({ results: (results && !results.error ? results : []),
+                          searching: false } )
+      })
+      .catch((message) => console.log(`There was an error, message: ${message}`))
+    }, 500)
   }
 
   render() {
     const { books, onChangeShelf } = this.props;
-    const { query, results } = this.state;
-
+    const { query, results, searching } = this.state;
     const tempResults = results.map(r => {
       books.forEach(b => {
         if(b.id === r.id)
@@ -58,14 +67,20 @@ class SearchBook extends Component {
             </div>
           )}
           <ol className="books-grid">
-            {tempResults.map((book) => (
-              <li key={book.id}>
-                <Book
-                  info={book}
-                  onChangeShelf={onChangeShelf}
-                />
-              </li>
-            ))}
+            {searching ? (
+                <div className="showing-results">
+                  <span>Loading...</span>
+                </div>
+              ) : (
+                tempResults.map((book) => (
+                  <li key={book.id}>
+                    <Book
+                      info={book}
+                      onChangeShelf={onChangeShelf}
+                    />
+                  </li>))
+              )
+            }
           </ol>
         </div>
       </div>
